00a9b47e5c2cf86e9681653a42c497dd5b39641f
David Tinianow <david.tinianow@gmail.com>
2016-12-20 22:38:04 -0700

    Remove unnecessary parser methods

--- src/main/java/salestaxes/Parser.java
+++ src/main/java/salestaxes/Parser.java
@@ -6,22 +6,10 @@ public final class Parser {
         String[] itemAndPrice = data.split(" at ");
         String[] quantityAndDescription = itemAndPrice[0].split(" ", 2);
 
-        String description = findDescription(quantityAndDescription);
-        int quantity = findQuantity(quantityAndDescription);
-        double price = findPrice(itemAndPrice);
+        String description = quantityAndDescription[1];
+        int quantity = Integer.parseInt(quantityAndDescription[0]);
+        double price = Double.parseDouble(itemAndPrice[1]);
 
         return new Item(description, quantity, price);
-    }
-
-    private static String findDescription(String[] data) {
-        return data[1];
-    }
-
-    private static int findQuantity(String[] data) {
-        return Integer.parseInt(data[0]);
-    }
-
-    private static double findPrice(String[] data) {
-        return Double.parseDouble(data[1]);
     }
 }
