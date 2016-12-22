2801242bef2068f5c9a1f4167425b1f1a0f3663e
David Tinianow <david.tinianow@gmail.com>
2016-12-21 17:35:57 -0700

    Fix tax calculator tests

--- src/test/java/salestaxes/TaxCalculatorTest.java
+++ src/test/java/salestaxes/TaxCalculatorTest.java
@@ -3,106 +3,42 @@ package salestaxes;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import java.util.List;
-import java.util.ArrayList;
 
 public class TaxCalculatorTest {
 
     @Test
-    public void itShouldApplySalesTaxesToNonExemptAndNonImportedItem() {
-        List<Item> items = new ArrayList<>();
-        Item nonExemptAndNonImportedItem = new Item("balloons", 2, 1.00);
-        items.add(nonExemptAndNonImportedItem);
-        Receipt receipt = new Receipt(items);
+    public void itShouldCalculateSalesTaxesForNonExemptNonImportedItem() {
+        Item nonExemptNonImported = new Item("balloons", 2, 1.00);
+        double tax = TaxCalculator.calculateSalesTaxes(nonExemptNonImported);
 
-        assertEquals(0.0, nonExemptAndNonImportedItem.getTaxes(), 0.001);
-        assertEquals(0.0, receipt.getSalesTaxes(), 0.001);
-        assertEquals(0.0, receipt.getTotalCost(), 0.001);
-
-        TaxCalculator.applySalesTaxes(receipt);
-
-        assertEquals(0.10, nonExemptAndNonImportedItem.getTaxes(), 0.001);
-        assertEquals(0.10, receipt.getSalesTaxes(), 0.001);
-        assertEquals(1.00, nonExemptAndNonImportedItem.getPrice(), 0.001);
-        assertEquals(1.10, receipt.getTotalCost(), 0.001);
+        assertEquals(0.10, nonExemptNonImported.getTaxes(), 0.001);
+        assertEquals(tax, 0.10, 0.001);
     }
 
     @Test
-    public void itShouldApplySalesTaxesToExemptAndNonImportedItem() {
-        List<Item> items = new ArrayList<>();
-        Item exemptAndNonImportedItem = new Item("chocolate", 2, 1.00);
-        items.add(exemptAndNonImportedItem);
-        Receipt receipt = new Receipt(items);
+    public void itShouldCalculateSalesTaxesForExemptNonImportedItem() {
+        Item exemptNonImported = new Item("chocolate", 2, 1.00);
+        double tax = TaxCalculator.calculateSalesTaxes(exemptNonImported);
 
-        assertEquals(0.0, exemptAndNonImportedItem.getTaxes(), 0.001);
-        assertEquals(0.0, receipt.getSalesTaxes(), 0.001);
-        assertEquals(0.0, receipt.getTotalCost(), 0.001);
-
-        TaxCalculator.applySalesTaxes(receipt);
-
-        assertEquals(0.00, exemptAndNonImportedItem.getTaxes(), 0.001);
-        assertEquals(0.00, receipt.getSalesTaxes(), 0.001);
-        assertEquals(1.00, exemptAndNonImportedItem.getPrice(), 0.001);
-        assertEquals(1.00, receipt.getTotalCost(), 0.001);
+        assertEquals(0.0, exemptNonImported.getTaxes(), 0.001);
+        assertEquals(tax, 0.0, 0.001);
     }
 
     @Test
-    public void itShouldApplySalesTaxesToExemptandImportedItem() {
-        List<Item> items = new ArrayList<>();
-        Item exemptAndImportedItem = new Item("imported chocolate", 2, 1.00);
-        items.add(exemptAndImportedItem);
-        Receipt receipt = new Receipt(items);
+    public void itShouldCalculateSalesTaxesForExemptImportedItem() {
+        Item exemptImported = new Item("imported chocolate", 2, 1.00);
+        double tax = TaxCalculator.calculateSalesTaxes(exemptImported);
 
-        assertEquals(0.0, exemptAndImportedItem.getTaxes(), 0.001);
-        assertEquals(0.0, receipt.getSalesTaxes(), 0.001);
-        assertEquals(0.0, receipt.getTotalCost(), 0.001);
-
-        TaxCalculator.applySalesTaxes(receipt);
-
-        assertEquals(0.05, exemptAndImportedItem.getTaxes(), 0.001);
-        assertEquals(0.05, receipt.getSalesTaxes(), 0.001);
-        assertEquals(1.00, exemptAndImportedItem.getPrice(), 0.001);
-        assertEquals(1.05, receipt.getTotalCost(), 0.001);
+        assertEquals(0.05, exemptImported.getTaxes(), 0.001);
+        assertEquals(tax, 0.05, 0.001);
     }
 
     @Test
-    public void itShouldApplySalesTaxesToNonExemptandImportedItem() {
-        List<Item> items = new ArrayList<>();
-        Item nonExemptAndImportedItem = new Item("imported balloons", 2, 1.00);
-        items.add(nonExemptAndImportedItem);
-        Receipt receipt = new Receipt(items);
+    public void itShouldCalculateSalesTaxesForNonExemptImportedItem() {
+        Item nonExemptImported = new Item("imported balloons", 2, 1.00);
+        double tax = TaxCalculator.calculateSalesTaxes(nonExemptImported);
 
-        assertEquals(0.0, nonExemptAndImportedItem.getTaxes(), 0.001);
-        assertEquals(0.0, receipt.getSalesTaxes(), 0.001);
-        assertEquals(0.0, receipt.getTotalCost(), 0.001);
-
-        TaxCalculator.applySalesTaxes(receipt);
-
-        assertEquals(0.15, nonExemptAndImportedItem.getTaxes(), 0.001);
-        assertEquals(0.15, receipt.getSalesTaxes(), 0.001);
-        assertEquals(1.00, nonExemptAndImportedItem.getPrice(), 0.001);
-        assertEquals(1.15, receipt.getTotalCost(), 0.001);
-    }
-
-    @Test
-    public void itShouldApplySalesTaxesToMultipleItems() {
-        List<Item> items = new ArrayList<>();
-        Item itemA = new Item("balloons", 2, 1.00);
-        Item itemB = new Item("imported chocolate", 2, 1.00);
-        items.add(itemA);
-        items.add(itemB);
-        Receipt receipt = new Receipt(items);
-
-        assertEquals(0.0, itemA.getTaxes(), 0.001);
-        assertEquals(0.0, itemB.getTaxes(), 0.001);
-        assertEquals(0.0, receipt.getSalesTaxes(), 0.001);
-        assertEquals(0.0, receipt.getTotalCost(), 0.001);
-
-        TaxCalculator.applySalesTaxes(receipt);
-
-        assertEquals(0.10, itemA.getTaxes(), 0.001);
-        assertEquals(0.05, itemB.getTaxes(), 0.001);
-        assertEquals(0.15, receipt.getSalesTaxes(), 0.001);
-        assertEquals(2.15, receipt.getTotalCost(), 0.001);
+        assertEquals(0.15, nonExemptImported.getTaxes(), 0.001);
+        assertEquals(tax, 0.15, 0.001);
     }
 }
