531582360cb00b0619d6d9ae5442bf85b56dccb0
David Tinianow <david.tinianow@gmail.com>
2016-12-21 17:42:49 -0700

    Add formatter test

--- src/test/java/salestaxes/FormatterTest.java
+++ src/test/java/salestaxes/FormatterTest.java
@@ -1,6 +1,6 @@
 package salestaxes;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import java.util.List;
@@ -8,18 +8,18 @@ import java.util.ArrayList;
 
 public class FormatterTest {
 
-//    @Test
-//    public void itShouldFormatReceipt() {
-//        List<Item> items = new ArrayList<>();
-//        Item itemA = new Item("balloons", 2, 1.00);
-//        Item itemB = new Item("imported chocolate", 1, 2.50);
-//        items.add(itemA);
-//        items.add(itemB);
-//        Receipt receipt = new Receipt(items);
-//
-//        String receiptText = Formatter.formatReceipt(receipt);
-//        String expected = "2 balloons: 1.10\n2 imported chocolate: 2.50\nSales Taxes: 0.00\nTotal: 2.32";
-//
-//        assertEquals(expected, receiptText);
-//    }
+    @Test
+    public void itShouldFormatReceipt() {
+        List<Item> items = new ArrayList<>();
+        Item itemA = new Item("balloons", 2, 1.00);
+        Item itemB = new Item("imported chocolate", 1, 2.50);
+        items.add(itemA);
+        items.add(itemB);
+        Receipt receipt = new Receipt(items);
+
+        String receiptText = Formatter.formatReceipt(receipt);
+        String expected = "2 balloons: 1.10\n1 imported chocolate: 2.65\nSales Taxes: 0.25\nTotal: 3.75";
+
+        assertEquals(expected, receiptText);
+    }
 }
