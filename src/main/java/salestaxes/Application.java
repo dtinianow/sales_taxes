88f99f4e94291dfcf32d7bbcd0285c8d553fcb84
David Tinianow <david.tinianow@gmail.com>
2016-12-23 13:54:30 -0700

    Modify application to accept multiple text files at once

--- src/main/java/salestaxes/Application.java
+++ src/main/java/salestaxes/Application.java
@@ -5,9 +5,17 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        List<Item> items = MyFileReader.readItemData(args[0]);
-        Receipt receipt = new Receipt(items);
-        String receiptText = Formatter.formatReceipt(receipt);
-        MyFileWriter.printReceipt(receiptText);
+        validateArgs(args);
+
+        for (String file : args) {
+            List<Item> items = MyFileReader.readItemData(file);
+            String receiptText = Formatter.formatReceipt(new Receipt(items));
+            MyFileWriter.printReceipt(receiptText);
+        }
+
+    }
+
+    private static void validateArgs(String[] args) {
+        if (args.length < 1) { throw new IllegalArgumentException("Please enter one or more files to be read"); }
     }
 }
